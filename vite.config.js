9f5796c18348a97ac8eb0e4b780152974c069bb2
Jin Kwan Kim <kkole3897@gmail.com>
2025-03-26 00:33:18 +0900

    chore: loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-  },
-  base: process.env.VITE_BASE_URL ?? "./",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        hash: "./index.hash.html",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    },
+    base: env.VITE_BASE || "./",
+    build: {
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+          hash: "./index.hash.html",
+        },
       },
     },
-  },
+  };
 });
